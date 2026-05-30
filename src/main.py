fe47debdec5437220aec163480498e7c391737d1
DJAGBA <veroniquedjagba@gmail.com>
2026-05-30 21:30:43 +0000

    Refactor lambda_handler to improve DynamoDB interaction and streamline S3 event processing

--- src/main.py
+++ src/main.py
@@ -6,22 +6,25 @@ TABLE_NAME = os.getenv("DYNAMODB_TABLE_NAME")
 
 def lambda_handler(event, context):
     print("Event: ", json.dumps(event))
-    for r in event.get("Records"):
-        if "object" in r.get("s3"):
-            filename = r["s3"]["object"]["key"]
-            print(filename)
-            
-    # Check database if filename exists
-    client = boto3.resource('dynamodb')
     
+    client = boto3.resource('dynamodb')
     table = client.Table(TABLE_NAME)
     
-    response = table.get_item(Key={"PK": "kdjagba"})
-    
-    item = {"PK": "KDJAGBA", "filename": "test"}
-    table.put_item(Item=item)
-    print(f"Item {item}")
-    
+    for r in event.get("Records", []):
+        if "s3" in r:
+            filename = r["s3"]["object"]["key"]
+            print(filename)        
+            
+            # Check database if filename exists
+            response = table.get_item(Key={"PK": filename})
+            
+            if "Item" in response:
+                print(f"Le fichier {filename} existe deja.")
+            else:
+                item = {"PK": filename, "filename": filename}
+                table.put_item(Item=item)
+                print(f"Item {item}")
+                
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda loaded from src/main.py!')
